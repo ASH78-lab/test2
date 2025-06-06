c1a00c73ffca5d1cf6ffca361472f389acea5ce8
ASH78-lab <roben4300@gmail.com>
2025-06-06 22:10:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -152,11 +152,10 @@ start2= datetime.strptime(start2, '%d/%m/%y %H:%M:%S')
 print(start2)
 
 
-TOKEN2 = os.environ.get('TOKEN2').replace(r'\n', '\n')
+TOKEN2 = os.getenv('TOKEN2')
 
-import json
-credentials={
-  "type": "service_account",
+
+credentials={"type": "service_account",
   "project_id": "avid-stone-461407-q5",
   "private_key_id": "440d864051de61f4b6463f10f8006898192b7420",
   "private_key": TOKEN2,
