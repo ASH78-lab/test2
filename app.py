22024cc26da684a96acff4b92792390f59e984e2
ASH78-lab <roben4300@gmail.com>
2025-06-07 11:12:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ import pandas as pd
 from selenium.common.exceptions import NoSuchElementException
 
 
-driver.set_window_size(1800, 1000) 
+
 
 
 
@@ -153,7 +153,27 @@ start2=f+'/'+d+'/'+c+' '+io
 start2= datetime.strptime(start2, '%d/%m/%y %H:%M:%S')
 print(start2)
 
+TOKEN2 = os.getenv('TOKEN2')
+TOKEN1="440d864051de61f4b6463f10f8006898192b7420"
+TOKEN3="ash789@avid-stone-461407-q5.iam.gserviceaccount.com"
+TOKEN4 ="116197129399001621585"
+TOKEN5="https://www.googleapis.com/robot/v1/metadata/x509/ash789%40avid-stone-461407-q5.iam.gserviceaccount.com"
 
+credentials={
+  "type": "service_account",
+  "project_id": "avid-stone-461407-q5",
+  "private_key_id": TOKEN1,
+  "private_key": TOKEN2,
+  "client_email": TOKEN3,
+  "client_id": TOKEN4,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": TOKEN5,
+  "universe_domain": "googleapis.com"
+}
+import gspread
+gc = gspread.service_account_from_dict(credentials)
 
 
 
