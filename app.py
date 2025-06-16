0f5f17bec259f7e10851313308e6b60e4a4139d0
ASH78-lab <roben4300@gmail.com>
2025-06-16 10:36:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
-import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
+from selenium.webdriver import Chrome
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
 
-import time
-from selenium.webdriver.common.keys import Keys
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-extensions')
+options.add_argument('--start-maximized')
+# Memory optimization
+options.add_argument('--disk-cache-size=1')
+options.add_argument('--media-cache-size=1')
+options.add_argument('--incognito')
+options.add_argument('--remote-debugging-port=9222')
+options.add_argument('--aggressive-cache-discard')
 
-from datetime import datetime
-import os
-date_new53 = str(datetime.now())
-print(date_new53)
-a123=time.time()
-
-
-
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-driver = webdriver.Chrome(options=chrome_options)
+service = Service('/usr/local/bin/chromedriver')
+    
+driver = webdriver.Chrome(service=service, options=options)
 
 
 
