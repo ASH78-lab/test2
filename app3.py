512f048ff2416e98a8cbd21a76bab6f3c068d0db
ASH78-lab <roben4300@gmail.com>
2026-01-20 12:51:56 +0300

    Update app3.py

--- app3.py
+++ app3.py
@@ -56,9 +56,13 @@ def whos():
     import undetected_chromedriver as uc
     from selenium import webdriver
     
-    options = webdriver.ChromeOptions() 
-    options.add_argument("start-maximized")
-    driver = uc.Chrome(options=options)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("start-maximized")
+  
+    driver = uc.Chrome(options=chrome_options)
   
     driver.get("https://1xbet.whoscored.com/") 
     time.sleep(15)
