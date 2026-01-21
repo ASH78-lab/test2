0ed3edad2c951ea9b3c03b06f55154e36398c575
ASH78-lab <roben4300@gmail.com>
2026-01-21 12:38:50 +0300

    Update app3.py

--- app3.py
+++ app3.py
@@ -1,14 +1,13 @@
 
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-
-service = Service()
-options = webdriver.ChromeOptions()
 
 
 
-driver = uc.Chrome(headless=True,use_subprocess=False,service=service, options=options, version_main=143)
+
+
+
+driver = uc.Chrome(headless=True,use_subprocess=False, version_main=143)
 
 driver.set_window_size(1800, 1000)
 driver.get("https://1xbet.whoscored.com/") 
