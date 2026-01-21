7c43b1a93f4cde56a807e5bd3a45638cc78e3615
ASH78-lab <roben4300@gmail.com>
2026-01-21 12:40:26 +0300

    Update app3.py

--- app3.py
+++ app3.py
@@ -1,26 +1,11 @@
 
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
-
-
-
-
-
-
+import time
 driver = uc.Chrome(headless=True,use_subprocess=False, version_main=143)
-
 driver.set_window_size(1800, 1000)
 driver.get("https://1xbet.whoscored.com/") 
 time.sleep(15)
-
-
-
-
-
-
-
-
-
 a=driver.find_element(By.TAG_NAME,'body')
 print(a.text)
     
