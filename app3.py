3830fab61f959e985539cd00f0ddb8c0d0c99de6
ASH78-lab <roben4300@gmail.com>
2026-01-21 12:45:34 +0300

    Update app3.py

--- app3.py
+++ app3.py
@@ -10,7 +10,7 @@ options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
-driver = uc.Chrome(options=option, version_main=143)
+driver = uc.Chrome(options=options, version_main=143)
 
 
 import time
