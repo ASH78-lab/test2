a7b70fdd300bf4ecfca43fa7c30c46d2c87e2c30
ASH78-lab <roben4300@gmail.com>
2026-01-27 12:31:20 +0300

    Update app3.py

--- app3.py
+++ app3.py
@@ -24,7 +24,7 @@ import time
 driver = uc.Chrome(headless=True,use_subprocess=False, version_main=143)
 driver.set_window_size(1800, 1000)
 '''
-driver.get("https://1xbet.whoscored.com/") 
+driver.get("https://whoscored.com/") 
 time.sleep(15)
 a=driver.find_element(By.TAG_NAME,'body')
 print(a.text)
