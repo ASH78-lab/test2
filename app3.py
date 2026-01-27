5e8ddef5887522a305a3e9d165cc81a936749349
ASH78-lab <roben4300@gmail.com>
2026-01-27 11:23:05 +0300

    Update app3.py

--- app3.py
+++ app3.py
@@ -10,6 +10,12 @@ options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
+
+custom_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299"
+options.add_argument(f'--user-agent={custom_user_agent}')
+
+
+
 driver = uc.Chrome(options=options, version_main=143)
 
 
