3bf4f765237fca4f663917e3afd9c43957417f8e
ASH78-lab <roben4300@gmail.com>
2026-09-11 12:52:06 +0300

    Update app5.py

--- app5.py
+++ app5.py
@@ -53,6 +53,7 @@ def asi78():
    
     URL = 'https://tipsters.asianbookie.com/?classic=1'
     req = requests.get(URL, headers=headers)
+    print(req.status_code)
     
     src = req.text
     soup = BeautifulSoup(src, 'lxml')
