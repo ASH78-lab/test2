117b6583a563744046c40087040751971fcf3011
ASH78-lab <roben4300@gmail.com>
2026-09-11 12:53:57 +0300

    Update app5.py

--- app5.py
+++ app5.py
@@ -45,14 +45,17 @@ def asi78():
 
     from bs4 import BeautifulSoup
     headers = {
-        'accept': '*/*',
-       'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-        'Referer': 'habr.com'
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Referer": "https://www.google.com/",
+            "Connection": "keep-alive",
         }
+
     
    
     URL = 'https://tipsters.asianbookie.com/?classic=1'
-    req = requests.get(URL, headers=headers)
+    req = requests.get(URL, headers=headers, timeout=10)
     print(req.status_code)
     
     src = req.text
